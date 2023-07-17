3cf31c7b64a7260f989fc1813d6a20676b0b9c60
Parker McMullin <parker.mcmullin01@gmail.com>
2023-07-17 13:22:26 -0700

    Add Code Mirror component with CSS support.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import CodeMirror from '$lib/components/CodeMirror.svelte';
+	import { css } from '@codemirror/lang-css';
+
+	let code = '';
+</script>
+
+<CodeMirror bind:value={code} lang={css()} />
