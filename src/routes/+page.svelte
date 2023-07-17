78eee57caa7bfda92026d5cfc9ad9dcdb70102ba
Parker McMullin <parker.mcmullin01@gmail.com>
2023-07-17 13:52:47 -0700

    Get injected styles to work.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,30 @@
 <script lang="ts">
 	import CodeMirror from '$lib/components/CodeMirror.svelte';
 	import { css } from '@codemirror/lang-css';
-	import { oneDark } from "@codemirror/theme-one-dark";
+	import { oneDark } from '@codemirror/theme-one-dark';
 
-	let code = '';
+	let code = `
+.ball {
+	width: 100px;
+	height: 100px;
+	border-radius: 50%;
+	background-color: #fff;
+}
+	`;
 </script>
 
-<h1 class="text-4xl font-bold dark:text-slate-50 mx-4 my-8 text-center">Style By Number</h1>
+<h1 class="text-4xl font-bold mx-4 my-8 text-center">Style By Number</h1>
 
 <section id="editor" class="mt-20 mx-8">
 	<CodeMirror bind:value={code} lang={css()} theme={oneDark} />
+</section>
+
+<section id="preview" class="mt-20 mx-8">
+	{@html `<style>${code}</style>`}
+
+	<div id="snowman">
+		<div id="ball-1" class="ball" />
+		<div id="ball-2" class="ball" />
+		<div id="ball-3" class="ball" />
+	</div>
 </section>
