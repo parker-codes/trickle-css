409f5299edee1f8b35878760e7bd83db97cdcb9b
Parker McMullin <parker.mcmullin01@gmail.com>
2023-07-17 14:02:06 -0700

    Add instructions.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,6 +18,28 @@
 	<CodeMirror bind:value={code} lang={css()} theme={oneDark} />
 </section>
 
+<section id="instructions" class="mt-20 mx-8">
+	<h2 class="text-2xl font-bold mb-4">Instructions</h2>
+	<p>
+		Using the <code class="text-slate-50">.ball</code> class, style the three balls to look like a snowman.
+	</p>
+
+	<ul class="mt-4">
+		<li>
+			<span class="text-green-400">✓</span>
+			Increase the width of the snowballs
+		</li>
+		<li>
+			<span class="opacity-20">✓</span>
+			Use `aspect-ratio` to make all dimensions the same instead of using `height`
+		</li>
+		<li>
+			<span class="opacity-20">✓</span>
+			Give them a nice, fully-round `border-radius`
+		</li>
+	</ul>
+</section>
+
 <section id="preview" class="mt-20 mx-8">
 	{@html `<style>${code}</style>`}
 
