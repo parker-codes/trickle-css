26caccb471e61744b04a99cd8c06007a3d02b1b0
Parker McMullin <parker.mcmullin01@gmail.com>
2023-07-26 11:41:07 -0700

    Make checkmarks lighter.

--- src/routes/challenges/[slug]/+page.svelte
+++ src/routes/challenges/[slug]/+page.svelte
@@ -98,7 +98,7 @@
 		<ul class="mt-4">
 			{#each verifiedTasks as task}
 				<li class="flex gap-2 mt-1 first:mt-0">
-					<span class={task.completed ? 'text-green-400' : 'opacity-20'}>✓</span>
+					<span class={task.completed ? 'text-green-300' : 'opacity-20'}>✓</span>
 					<span>{task.text}</span>
 				</li>
 			{/each}
